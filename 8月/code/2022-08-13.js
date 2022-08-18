5408e3e9d86eb24d8d998d55cd64e5b91b0b7115
Haohao-555 <3283627490@qq.com>
2022-08-18 20:36:54 +0800

    补

--- 8月/code/2022-08-13.js
+++ 8月/code/2022-08-13.js
@@ -21,3 +21,50 @@
   }
   return slow
 };
+
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number[]}
+ */
+ var findSubstring = function(s, words) {
+    if (words.length == 0) return [];
+ 
+    // 计算单词总长度
+    let wordsLen = words.length * words[0].length;
+    if (s.length < wordsLen) return  [];
+ 
+    let resArr = [];
+    // 每个单词长度
+    let childLen = words[0].length;
+ 
+    // 拷贝
+    let wordsCopy;
+    for (let i = 0; i <= s.length - wordsLen; i++) {
+        // 从匹配串截取 单词总长度 的字符串
+        let curS = s.slice(i, i+wordsLen);
+        console.log("curS字符串为", curS);
+ 
+        let is_Target = true;
+        wordsCopy = [...words];
+ 
+        for (let j = 0; j <= wordsLen-childLen; j+= childLen) {
+            // 截取第 j 个单词出来
+            let curChild = curS.slice(j, j+childLen);
+            console.log(`从 curS 截取的第${j + 1}个单词为`, curChild);
+ 
+            // 判断在 wordsCopy 中是否存在
+            let index = wordsCopy.indexOf(curChild);
+            if (index !== -1) {
+                // 存在，移除
+                wordsCopy.splice(index, 1);
+            } else {
+                is_Target = false;
+                break;
+            }
+        }
+        console.log("-----------------");
+        if (is_Target) resArr.push(i);
+    }
+    return resArr;
+ };
--- 8月/code/2022-08-16.js
+++ 8月/code/2022-08-16.js
@@ -1,31 +1,46 @@
-var divide = function(dividend, divisor) {
-  if (divisor === 0) return Infinity;
-  if (dividend === 0) return 0;
-  if (dividend === -2147483648 && divisor === -1) return 2147483647;
+var divide = function (dividend, divisor) {
+    if (divisor === 0) return Infinity;
+    if (dividend === 0) return 0;
+    if (dividend === -2147483648 && divisor === -1) return 2147483647;
 
-  let res = 0;
-  let flag = '';
-  if (dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0) {
-      flag = '-';
-  }
-  dividend = Math.abs(dividend);
-  divisor = Math.abs(divisor);
-  
-  while (dividend >= divisor) {
-      let temp = divisor, m = 1;
+    let res = 0;
+    let flag = '';
+    if (dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0) {
+        flag = '-';
+    }
+    dividend = Math.abs(dividend);
+    divisor = Math.abs(divisor);
 
-      // 计算出最大值
-      while (temp <= (dividend >> 1)) { 
-          temp <<= 1; // temp = temp * 2;
-          m <<= 1; // m = m * 2;
-      }
+    while (dividend >= divisor) {
+        let temp = divisor, m = 1;
 
-      dividend -= temp;
-      res += m;
+        // 计算出最大值
+        while (temp <= (dividend >> 1)) {
+            temp <<= 1; // temp = temp * 2;
+            m <<= 1; // m = m * 2;
+        }
 
-      // dividend 可能还可以再减去多个 divisor，故再次执行一次 while
-  }
+        dividend -= temp;
+        res += m;
 
-  return parseInt(flag + res);
+        // dividend 可能还可以再减去多个 divisor，故再次执行一次 while
+    }
+
+    return parseInt(flag + res);
 };
 console.log(divide(1, 1))
+
+var searchInsert = function (nums, target) {
+    const n = nums.length;
+    let left = 0, right = n - 1, ans = n;
+    while (left <= right) {
+        let mid = ((right - left) / 2) + left;
+        if (target <= nums[mid]) {
+            ans = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return ans;
+};

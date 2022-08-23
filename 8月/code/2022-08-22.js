64fa32a74691f61093dcb81f960af5f4c307d9da
Haohao-555 <3283627490@qq.com>
2022-08-23 20:18:30 +0800

    K个一组翻转链表

--- 8月/code/2022-08-22.js
+++ 8月/code/2022-08-22.js
@@ -11,3 +11,52 @@ var addBinary = function(a, b) {
   if (add === 1) sum.unshift(1)
   return sum.join('')
 };
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+ var reverseKGroup = function(head, k) {
+  if (head == null) return head;
+  let start = head;
+  let end = head;
+
+  // start ~ end 反转链表
+  let reverse = (start, end) => {
+      let prevNode = end;
+      let currentNode = start;
+      let nextNode = start;
+      while(currentNode != end) {
+          let nextNode = currentNode.next;
+          
+          // 当前节点指向上一个节点
+          currentNode.next = prevNode;
+
+          // 指向下一个结点
+          currentNode = nextNode;
+          // 更新上一个结点
+          prevNode = currentNode;
+      }
+      // 返回链表的起始头
+      return prevNode;
+  }
+
+ //  获取 k 个结点 的起始头
+ for (let i = 0; i < k; i++) {
+        if (end == null) return head;
+        end = end.next;
+ }
+
+// 更新反转后链表的起始头
+let newHead = reverse(start, end);
+// 递归，并连接之前递归结果
+start.next = reverseKGroup(end, k);
+return newHead;
+};

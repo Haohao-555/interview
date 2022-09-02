c825a6a3fcf5e2188b6e1a446d24a987288873ac
Haohao-555 <3283627490@qq.com>
2022-09-02 21:15:39 +0800

    补：二叉树的中序遍历

---
+++ 8月/code/2022-08-26.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root, array = []) {
+  if (root) {
+    inorderTraversal(root.left, array);
+    array.push(root.val);
+    inorderTraversal(root.right, array);
+  }
+  return array;
+
+};

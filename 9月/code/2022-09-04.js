8fda2ad3ec8ccb60018367e51add92450373182a
Haohao-555 <3283627490@qq.com>
2022-09-04 20:33:06 +0800

    字符串相乘

--- 9月/code/2022-09-04.js
+++ 9月/code/2022-09-04.js
@@ -26,3 +26,35 @@ var firstMissingPositive = function (nums) {
   }
   return len + 1;
 };
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function (num1, num2) {
+  if (num1 == '0' || num2 == '0') return '0';
+
+  let len1 = num1.length;
+  let len2 = num2.length;
+
+  // 结果数组
+  let res = new Array(len1 + len2).fill(0);
+
+  for (let i = len1 - 1; i >= 0; i--) {
+    for (let j = len2 - 1; j >= 0; j--) {
+      const mul = num1[i] * num2[j];
+      // 乘积在结果数组的位置
+      const p1 = j + i;
+      // 进位位置
+      const p2 = i + j + 1;
+
+      const sum = res[p2] + mul;
+      res[p1] += Math.floor(sum / 10);
+      res[p2] = sum % 10
+    }
+  }
+
+  if (res[0] == 0) res.shift()
+  return res.join("")
+};

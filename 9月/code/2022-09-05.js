1dc6ddbc7bc88601a56836234ad5eadb8a46b97c
Haohao-555 <3283627490@qq.com>
2022-09-05 19:32:27 +0800

    跳跃游戏

---
+++ 9月/code/2022-09-05.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+  let len = nums.length;
+  // 距离
+  let distance = len - 1;
+  // 倒数第二个开始
+  let last = len - 2;
+
+  for (let i = last; i >= 0; i--) {
+    // nums[i] + i 代表当前位置（i）能够到达最远右边的距离
+    if (nums[i] + i >= distance) {
+      // 进入到这里，说明当前 i 可以到达最右边
+
+      // 接着distance更新为（最左边到达i的距离） 
+      distance = i;
+
+      // 判断i之前有哪些点可以到达 i，由此循环
+    }
+  }
+  // 遍历完数组， distance 为 0 说明可以进行跳跃
+  return distance === 0;
+}

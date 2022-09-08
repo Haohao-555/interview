f8cd05f96b4c64db545b9b73b1c5be3d11818419
Haohao-555 <3283627490@qq.com>
2022-09-08 20:33:52 +0800

    最大子数组和

--- 9月/code/2022-09-08.js
+++ 9月/code/2022-09-08.js
@@ -12,3 +12,21 @@ var myPow = function (x, n) {
     return n % 2 == 0 ? myPow(x * x, n / 2) : myPow(x * x, Math.floor(n / 2)) * x
   }
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let ans = nums[0];
+  let sum = 0;
+  for (const num of nums) {
+    if (sum > 0) {
+      sum += num;
+    } else {
+      sum = num;
+    }
+    ans = Math.max(ans, sum);
+  }
+  return ans;
+};

2a4f2354ce184a1140f8438f29d2f4e1f7b0ca9f
Haohao-555 <3283627490@qq.com>
2022-09-14 19:50:34 +0800

    不同路径II

--- 9月/code/2022-09-11.js
+++ 9月/code/2022-09-11.js
@@ -6,15 +6,42 @@
  *     （2）从下方向过来的（即从 dp[i - 1][j] 过来） 
  * 故路径总和 dp[i][j] = dp[i][j - 1] + dp[i -1][j];
  */
- var uniquePaths = function(m, n) {
+var uniquePaths = function (m, n) {
   let dp = new Array(m).fill(new Array(n).fill(1));
- 
+
   for (let i = 1; i < m; i++) {
-      for (let j = 1; j < n; j++) {
-          dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-      }
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
   }
 
   return dp[m - 1][n - 1];
 }
 console.log(uniquePaths(3, 7));
+/**
+ * 道理跟上述一致，只不过需要判断该位置是否有障碍，若有障碍 则 dp[i][j] = 0;
+ */
+var uniquePathsWithObstacles = function (obstacleGrid) {
+  let m = obstacleGrid.length
+  let n = obstacleGrid[0].length
+  let dp = Array(m).fill().map(item => Array(n).fill(0))
+
+  //初始化
+  for (let i = 0; i < m && obstacleGrid[i][0] === 0; i++) {
+    dp[i][0] = 1
+  }
+  for (let j = 0; j < n && obstacleGrid[0][j] === 0; j++) {
+    dp[0][j] = 1
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      if (obstacleGrid[i][j] === 0) {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+      } else {
+        dp[i][j] = 0
+      }
+    }
+  }
+  return dp[m - 1][n - 1]
+};
+console.log(uniquePathsWithObstacles([[0], [1]]))

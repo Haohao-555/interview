7bbc6377364c894733fc9d9c2d2db87a7e6034b0
Haohao-555 <3283627490@qq.com>
2022-09-15 19:41:33 +0800

    矩阵置零

--- 9月/code/2022-09-12.js
+++ 9月/code/2022-09-12.js
@@ -26,3 +26,34 @@ console.log(simplifyPath("/a/./b/../../c/"));
 console.log(simplifyPath("/home//foo/"));
 console.log(simplifyPath("/a/../../b/../c//.//"));
 console.log(simplifyPath("/a//bc/d//././/.."));
+
+
+var setZeroes = function (matrix) {
+  let row = matrix.length;
+  let col = matrix[0].length;
+
+  let rowArr = new Array(row).fill(false);
+  let colArr = new Array(col).fill(false);
+
+  // 将 matrix 中存在 0 的位置记录到 行数组（rowArr) 列数组（colArr）
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (matrix[i][j] == 0) {
+        rowArr[i] = true;
+        colArr[j] = true;
+      }
+    }
+  }
+
+  // 更新
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (rowArr[i] || colArr[j]) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  return matrix;
+}
+console.log(setZeroes([[1, 1, 1], [1, 0, 1], [1, 1, 1]]));

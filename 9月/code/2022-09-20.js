057c5b96183326acb282ef73e17e010fcde93075
Haohao-555 <3283627490@qq.com>
2022-09-20 19:49:18 +0800

    子集II

--- 9月/code/2022-09-20.js
+++ 9月/code/2022-09-20.js
@@ -11,3 +11,29 @@ let removeDuplicates = function (nums) {
   return len;
 }
 console.log(removeDuplicates([1, 1, 1, 2, 2, 3]));
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function (nums) {
+  const result = [];
+  nums.sort((a, b) => {
+    return a - b;
+  });
+
+  let backtrack = (start, curr) => {
+    result.push([...curr]);
+    for (let i = start; i < nums.length; i++) {
+      if (i > start && nums[i] == nums[i - 1]) {
+        continue;
+      }
+
+      curr.push(nums[i]);
+      backtrack(i + 1, curr);
+      curr.pop();
+    }
+  }
+  backtrack(0, []);
+  return result;
+};

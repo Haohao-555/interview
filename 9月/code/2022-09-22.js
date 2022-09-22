0a721345c81785d38301e561f8886e0ca740864c
Haohao-555 <3283627490@qq.com>
2022-09-22 19:44:48 +0800

    柱状图中最大的矩阵

--- 9月/code/2022-09-22.js
+++ 9月/code/2022-09-22.js
@@ -18,3 +18,23 @@ var wordBreak = function (s, wordDict) {
 let s = `applepenapple`;
 let wordDict = ['apple', 'pen'];
 console.log(wordBreak(s, wordDict));
+
+var largestRectangleArea = function (heights) {
+  let large = 0;
+  let shift = [{ index: -1, height: -1 }];
+  for (let i = 0; i < heights.length; i++) {
+    // 队列中最后一个元素为非初始化元素
+    while (shift[shift.length - 1].height !== -1 && shift[shift.length - 1].height > heights[i]) {
+      const pop = shift.pop();
+      large = Math.max(large, pop.height * (i - 1 - shift[shift.length - 1].index));
+    }
+    shift.push({ index: i, height: heights[i] });
+  }
+  const top = shift[shift.length - 1].index;
+  // 
+  while (shift[shift.length - 1].height !== -1) {
+    const pop = shift.pop();
+    large = Math.max(large, pop.height * (top - shift[shift.length - 1].index));
+  }
+  return large
+};
